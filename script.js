d6165f0891295a2e33d7d55bc5d2ad0488335153
abouillon <a.bouillon21@gmail.com>
2015-08-18 15:47:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@
         // rotate around this point
         
         if(width > height){
-            ctx.translate(-width * 1.5, -height);
+            ctx.translate(-height, -width);
             ctx.rotate(90 * Math.PI / 180);
         }
         
