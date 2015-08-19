0f958f780026670184aaba8c1b61e6dc03047665
abouillon <a.bouillon21@gmail.com>
2015-08-19 09:38:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -112,7 +112,7 @@
         image.src = base64data;
         image.onload = function() {
             ctx.translate(image.width, image.height);
-            ctx.rotate(90 * Math.PI / 180);
+            ctx.rotate(180 * Math.PI / 180);
             ctx.drawImage(image, 0, 0); 
         };
 
