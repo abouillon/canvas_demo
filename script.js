14d5a5b79d23b9d1c8fe47a84c16c0e22ed82bc6
abouillon <a.bouillon21@gmail.com>
2015-08-19 12:32:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@
                 
                 newDataUri = imageToDataUri(this, width, height, quality);
                 if(/iPad|iPhone|iPod/.test(navigator.platform)){
-                    if(img.width > img.height){
+                    if(img.height > img.width){
                         rotateBase64Image(newDataUri, quality, width, height);
                     } else {
                         img2.src = newDataUri;
