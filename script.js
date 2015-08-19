7baf31b68edab99f3331ae1ae732707e3600cc1d
abouillon <a.bouillon21@gmail.com>
2015-08-19 12:48:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -129,7 +129,7 @@
             var image = new Image();
             image.src = base64data;
             image.onload = function(){
-                ctx.translate(height, 0);
+                ctx.translate(width, 0);
                 ctx.rotate(90 * Math.PI / 180);
                 ctx.drawImage(image, 0, 0, width, height);
                 img2.src = canvas.toDataURL('image/jpeg', quality || 0.8);
