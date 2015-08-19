76067e955e99c49842260057da24b4c3b945b734
abouillon <a.bouillon21@gmail.com>
2015-08-19 10:30:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -115,7 +115,7 @@
         image.onload = function(){
             ctx.translate(image.width, image.height);
             ctx.rotate(180 * Math.PI / 180);
-            drawImageIOSFix(ctx, img, 0, 0, image.width, image.height, 0, 0, width, height);
+            drawImageIOSFix(ctx, image, 0, 0, image.width, image.height, 0, 0, width, height);
             img2.src = canvas.toDataURL('image/jpeg', quality || 0.8);
         }
     }
